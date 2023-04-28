7cbbded2601474fe75126acada99fe57447a7423
jakobakoba <jakobakoba@gmail.com>
2023-04-28 18:19:17 +0600

    extract method

--- kotlin/src/main/AuthorNameNormalizer.kt
+++ kotlin/src/main/AuthorNameNormalizer.kt
@@ -2,17 +2,22 @@ class AuthorNameNormalizer() {
 
     fun normalize(name: String): String {
         val splitName = name.trim().split(" ")
+        if (splitName.count() == 1) return name
 
+        val middleInitial = middleInitial(splitName)
+
+        return "${splitName.last()}, ${splitName.first()}${middleInitial}"
+    }
+
+    private fun middleInitial(splitName: List<String>): String {
         var middleName = ""
-        if (splitName.count() >= 3){
+        if (splitName.count() >= 3) {
             middleName = splitName[1]
         }
         var middleInitial = ""
-        if (!middleName.isEmpty()){
+        if (!middleName.isEmpty()) {
             middleInitial = " " + middleName.first() + "."
         }
-
-        if (splitName.count() == 1) return name
-        return "${splitName.last()}, ${splitName.first()}${middleInitial}"
+        return middleInitial
     }
 }
