8e3015944ebfe94769e8e46b04403cef74ce703f
jakobakoba <jakobakoba@gmail.com>
2023-04-30 23:28:44 +0600

    flip conditional

--- kotlin/src/main/AuthorNameNormalizer.kt
+++ kotlin/src/main/AuthorNameNormalizer.kt
@@ -1,16 +1,19 @@
 class AuthorNameNormalizer() {
 
     fun normalize(name: String): String {
-        val suffixSplit = name.trim().split(",")
-        require(suffixSplit.count() <= 2) { "Can't handle more than 1 comma" }
-        val splitName = suffixSplit.first().split(" ")
-        if (splitName.count() == 1) return name
-        val suffix = if (suffixSplit.count() > 1) ",${suffixSplit.last()}" else ""
-        return "${splitName.last()}, ${splitName.first()}${middleInitials(splitName)}$suffix"
+        val prefixSuffix = name.trim().split(",")
+        require(prefixSuffix.count() <= 2) { "Can't handle more than 1 comma" }
+        val nameParts = prefixSuffix.first().split(" ")
+        if (nameParts.count() == 1) return name
+        return "${nameParts.last()}, ${nameParts.first()}${middleInitials(nameParts)}${suffix(prefixSuffix)}"
     }
 
-    private fun middleInitials(splitName: List<String>): String {
-        val middleNames = splitName.drop(1).dropLast(1)
+    private fun suffix(prefixSuffix: List<String>): String {
+        return if (prefixSuffix.count() == 1) "" else  ",${prefixSuffix.last()}"
+    }
+
+    private fun middleInitials(nameParts: List<String>): String {
+        val middleNames = nameParts.drop(1).dropLast(1)
         return buildString {
             middleNames.forEach { append(initialize(it)) }
         }
