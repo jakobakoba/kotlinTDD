4f44a5f23b37752e7f5ab0637daeb51fbd48daf1
jakobakoba <jakobakoba@gmail.com>
2023-04-30 23:13:15 +0600

    test 9 passes

--- kotlin/src/main/AuthorNameNormalizer.kt
+++ kotlin/src/main/AuthorNameNormalizer.kt
@@ -1,11 +1,17 @@
+
+
 class AuthorNameNormalizer() {
 
     fun normalize(name: String): String {
         val suffixSplit = name.trim().split(",")
+        require(suffixSplit.count() <= 2) { "Can't handle more than 1 comma" }
         val splitName = suffixSplit.first().split(" ")
         if (splitName.count() == 1) return name
-        val suffix = if (suffixSplit.count() > 1) ",${suffixSplit.last()}" else ""
-        return "${splitName.last()}, ${splitName.first()}${middleInitials(splitName)}$suffix"
+        return "${splitName.last()}, ${splitName.first()}${middleInitials(splitName)}${suffix(suffixSplit)}"
+    }
+
+    private fun suffix(suffixSplit: List<String>): String {
+        return if (suffixSplit.count() > 1) ",${suffixSplit.last()}" else ""
     }
 
     private fun middleInitials(splitName: List<String>): String {
